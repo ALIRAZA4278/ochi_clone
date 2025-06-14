cff053ad1569a7416cf7ac9c10cda0bd7f632d84
Ali Raza <156216939+ALIRAZA4278@users.noreply.github.com>
2025-06-14 13:42:21 +0500

    ochi

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import About from "./Components/About"
 import Eyes from "./Components/Eyes"
 import LandingPage from "./Components/LandingPage"
 import Marquee from "./Components/Marquee"
-import Navbar from "./components/Navbar"
+ 
 
 const App = () => {
   return (
     <div className='w-full min-h-screen bg-zinc-900 text-white m-0 p-0 box-border'>
-      <Navbar/>
+
       <LandingPage/>
       <Marquee/>
       <About/>
